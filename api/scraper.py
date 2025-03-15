985f807606969837d4172b5e3459bfc7dc797ed9
Sorin Cruceru <sorin.cruceru03@gmail.com>
2025-03-15 19:37:35 +0000

    login and register routes with JWT. Gemini + web scraper test

---
+++ api/scraper.py
@@ -0,0 +1,35 @@
+import requests
+
+# scrape for 1 webpage
+
+def download_webpage(url, output_file):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad status codes
+        
+        with open(output_file, 'w', encoding='utf-8') as file:
+            file.write(response.text)
+        
+        print(f"Webpage saved to {output_file}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading the webpage: {e}")
+
+
+def text_webpage(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad status codes
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading the webpage: {e}")
+
+
+# Example usage
+
+
+if __name__=='__main__':
+    url = "https://www.example.com"
+    output_file = "webpage.txt"
+
+
+    download_webpage(url, output_file)
